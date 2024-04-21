08be6c05f6d661aad01c877eda8ed9c81f725bdd
kotokotodonabe <ksk.pleiades@gmail.com>
2024-04-21 20:58:04 +0900

    Update Puma Configuration

--- config/puma.rb
+++ config/puma.rb
@@ -8,6 +8,7 @@
 #
 workers Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
 
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -18,6 +19,7 @@ max_threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS") { 5 })
 min_threads_count = Integer(ENV.fetch("RAILS_MIN_THREADS") { max_threads_count })
 threads min_threads_count, max_threads_count
 
+
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
@@ -25,16 +27,22 @@ threads min_threads_count, max_threads_count
 #
 preload_app!
 
+
+rackup      DefaultRackup
+
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 3000 }
+
 
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RACK_ENV") { "development" }
 
+
 on_worker_boot do
-  # Worker-specific setup for Rails 4.1 to 5.2, after 5.2 it's not needed
+  # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
